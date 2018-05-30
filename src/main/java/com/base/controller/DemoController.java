27853ca843383fba3b6df787e8db57f3b75a7c30
wymin <cryenging@outlook.com>
2018-05-30 16:29:55 +0800

    修改 部分代码

--- src/main/java/com/base/controller/DemoController.java
+++ src/main/java/com/base/controller/DemoController.java
@@ -71,9 +71,22 @@ public class DemoController {
         return map;
     }
 
-    @RequestMapping(value = "/test")
-    public void test(){
-        System.out.println("normal");
+    /**
+     * RequestMapping method
+     * value : 指定请求的实际地址，指定的地址可以是URI Template 模式
+     * method : 指定请求的method类型， GET、POST、PUT、DELETE等
+     * params : 指定request中必须包含某些参数值时，才让该方法处理。
+     * headers :  指定request中必须包含某些指定的header值，才能让该方法处理请求。
+     * consumes : 指定处理请求的提交内容类型（Content-Type），例如application/json, text/html;
+     * produces :  指定返回的内容类型，仅当request请求头中的(Accept)类型中包含该指定类型才返回；
+     */
+    @RequestMapping(value = "/test",params = {"id","value!=1"}, headers = { "Accept-Language=US,zh;q=0.8" }, consumes = "application/json",produces = "application/json")
+    public @ResponseBody Map<String,Object> test(){
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("method","test");
+        System.out.println("test");
+        return map;
     }
+
 
 }
